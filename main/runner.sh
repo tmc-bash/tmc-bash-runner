9c9d308db2268148d6a5d98374d0346efce3dc0e
lchz <lauraz940829@hotmail.com>
2020-06-15 16:17:20 +0300

    added point attribute, results and available points also worked

--- main/runner.sh
+++ main/runner.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bats
+#/usr/bin/env bats
+
+. ./tmc/utils.sh
 
 TEST_PATH=`find ./ -name "test"`
 
@@ -8,21 +10,61 @@ for file in $TEST_PATH/*
     TEST_FILES+=("$file")
 done
 
-RESULTS_FILE="test_results.txt"
+RESULTS_FILE=".tmc_test_results.json"
 if [ -e $RESULTS_FILE ]
-  then
-    rm $RESULTS_FILE
+then
+  rm $RESULTS_FILE
 fi
 
-bats ${TEST_FILES[@]} > test_results.txt
-STATUS="$?"
+printf "[]" >> $RESULTS_FILE
 
-if [ $STATUS == 0 ]
-  then
-    printf "PASSED" >> test_results.txt
-else
-    printf "FAILED" >> test_results.txt
+
+TEMP_POINTS_FILE="temp_points.txt"
+TEMP_RESULTS_FILE="temp_results.txt"
+
+bats ${TEST_FILES[@]} >> $TEMP_RESULTS_FILE
+
+# Check if points sum and tests sum match
+count_points=$(< $TEMP_POINTS_FILE wc -l  )
+count_results=$(head -n 1 $TEMP_RESULTS_FILE)
+if [ $count_points != ${count_results:3:4} ]; then
+  printf "Error: Points sum does not match tests sum. Please check again.\n"
+  clear_temps
+  exit 1
 fi
 
+# Get points
+declare -a points_array
+while IFS= read -r line
+do
+  points_array+=($line)
+done < "$TEMP_POINTS_FILE"
 
+# Get names and status and print to json
+declare -a names_array
+declare -a status_array
 
+index=0
+while read line
+do
+  IFS=' ' read -ra array <<< "$line"
+
+  if grep -q '{' "$RESULTS_FILE"; then
+    punc=","
+  fi
+
+  status=true
+  start=3
+  if [[ $line == *"not ok"* ]]; then
+    status=false
+    start=4
+  fi
+    
+  name="$(cut -d' ' -f ${start}-${#array[@]} <<<"$line")"
+  sed -i "s/]$/${punc}{\"name\": \"${name}\", \"points\": [\"${points_array[$index]}\"], \"status\": ${status}, \"message\": \"\", \"backtrace\": []}]/" $RESULTS_FILE
+
+  index=$(($index+1))
+
+done <<< "$(grep -e 'ok' $TEMP_RESULTS_FILE)"
+
+clear_temps
